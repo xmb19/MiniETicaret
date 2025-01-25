68c2ac9f5e5fb01607b89556e3a20e1917ab6087
xmb19 <muratbektas019@gmail.com>
2025-01-25 14:49:46 +0300

    Add project files.

---
+++ Infastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretAPI.Application.Abstraction;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        => new()
+        {
+            new() { Id = Guid.NewGuid(),Name = "Laptop", Price = 1000, Stock = 10 },
+            new () { Id = Guid.NewGuid(), Name = "Phone", Price = 500, Stock = 10 },
+            new () { Id = Guid.NewGuid(), Name = "Tablet", Price = 800, Stock = 10 },
+            new () { Id = Guid.NewGuid(), Name = "PC", Price = 1500, Stock = 10 },
+        };
+    };
+}
+
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using ETicaretAPI.Application.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _productService.GetProducts();
+            return Ok(products);
+        }
+    }
+}
